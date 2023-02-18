9776ae065a782be9ff3edf667b6dabdd0f91aafc
AvMariam <mariam.avetisyan.2001@mail.ru>
2023-02-18 17:28:21 +0400

    1

--- craps.py
+++ craps.py
@@ -2,29 +2,39 @@ import random as rd
 
 def dice_sum(a, b):
     return a + b
+def game():
+    goal = 0
+    flag = 1
+    dice1 = rd.randint(1, 6)
+    dice2 = rd.randint(1, 6)
+    print(f"The sum of dice is {dice1} + {dice2} = {dice_sum(dice1, dice2)}")
+    if dice_sum(dice1, dice2) == 7 or dice_sum(dice1, dice2) == 11:
+        print("Congratulations!!! You WON.")
+    elif dice_sum(dice1, dice2) == 2 or dice_sum(dice1, dice2) == 3 or dice_sum(dice1, dice2) == 12:
+        print("I'm sorry, the casino won.")
+    else:
+        goal = dice_sum(dice1, dice2)
+        print(f"Now your goal is {goal}.")
+        while flag:
+            dice1 = rd.randint(1, 6)
+            dice2 = rd.randint(1, 6)
+            print(f"The sum of dice is {dice1} + {dice2} = {dice_sum(dice1, dice2)}")
+            if dice_sum(dice1, dice2) == goal:
+                print("You won.")
+                flag = 0
+            elif dice_sum(dice1, dice2) == 7:
+                print("You lose.")
+                flag = 0
 
-goal = 0
-flag = 1
-
-dice1 = rd.randint(1, 6)
-dice2 = rd.randint(1, 6)
-
-print(f"The sum of dice is {dice1} + {dice2} = {dice_sum(dice1, dice2)}")
-if dice_sum(dice1, dice2) == 7 or dice_sum(dice1, dice2) == 11:
-    print("Congratulations!!! You WON.")
-elif dice_sum(dice1, dice2) == 2 or dice_sum(dice1, dice2) == 3 or dice_sum(dice1, dice2) == 12:
-    print("I'm sorry, the casino won.")
+answer = input("Do you want to start the game?(yes/no): ")
+if answer.lower() == "yes":
+    game()
+    again = input("Do you want to play again?(yes/no): ")
+    while again.lower() == "yes":
+        game()
+        again = input("Do you want to play again?(yes/no): ") 
+    if again.lower() == "no":
+        print("Thank you for participation.")
 else:
-    goal = dice_sum(dice1, dice2)
-    print(f"Now your goal is {goal}.")
-    while flag:
-        dice1 = rd.randint(1, 6)
-        dice2 = rd.randint(1, 6)
-        print(f"The sum of dice is {dice1} + {dice2} = {dice_sum(dice1, dice2)}")
-        if dice_sum(dice1, dice2) == goal:
-            print("You won.")
-            flag = 0
-        elif dice_sum(dice1, dice2) == 7:
-            print("You lose.")
-            flag = 0
+    print("Your loss.")
 
